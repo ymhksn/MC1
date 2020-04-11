ae64f9a798db19610fcb0ef6829c70df29d46f62
Yohanes Markus <ymheksan@gmail.com>
2020-04-11 23:38:43 +0700

    Table View already separated but there's a bug when you updated the status check to uncheck and vice versa

--- MC1/Controllers/CategoryVC.swift
+++ MC1/Controllers/CategoryVC.swift
@@ -7,30 +7,77 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryVC: UIViewController {
     
     @IBOutlet weak var selectedCategoryTableView: UITableView!
     @IBOutlet weak var categoryTableView: UITableView!
- 
-    var selectedCategories = ["Please work", "I don't know what to do", "Yeay"]
-    var categories = ["Gym", "Work", "Sleep"]
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    //var to store data from CoreData DB temp
+    var selectedCategories = [Category]()
+    var categories = [Category]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //DataSource Protocol
+        //DataSource & Delegate Protocol
         selectedCategoryTableView.dataSource = self
+        selectedCategoryTableView.delegate = self
         categoryTableView.dataSource = self
+        categoryTableView.delegate = self
         
         //Register custom Nib for cell
         selectedCategoryTableView.register(UINib(nibName: "CategoryTVCell", bundle: nil), forCellReuseIdentifier: "reusableTVCell")
         categoryTableView.register(UINib(nibName: "CategoryTVCell", bundle: nil), forCellReuseIdentifier: "reusableTVCell")
+        
+        /*
+        //Save data to CoreData
+        let categories = Category(context: context)
+        categories.name = "Work"
+        categories.color = "red1"
+        saveCategory()
+        */
+        
+        loadCategory()
+        
     }
     
+//MARK: - CoreData Func
+    //CoreData Create
+    func saveCategory() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving to DB \(error)")
+        }
+    }
+    
+    //CoreData Read
+    func loadCategory() {
+        //Fetch Data
+        let requestCheck: NSFetchRequest<Category> = Category.fetchRequest()
+        let requestUncheck: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        //Query Data
+        let checkPredicate = NSPredicate(format: "checked MATCHES %@", "check")
+        let uncheckPredicate = NSPredicate(format: "checked MATCHES %@", "uncheck")
+        
+        requestCheck.predicate = checkPredicate
+        requestUncheck.predicate = uncheckPredicate
+  
+        do {
+            selectedCategories = try context.fetch(requestCheck)
+            categories = try context.fetch(requestUncheck)
+        } catch {
+            print("Error loading from DB \(error)")
+        }
+    }
 }
 
-//MARK: - Data Source
+//MARK: - TableView DataSource
 extension CategoryVC: UITableViewDataSource{
     func tableView(_ tableView: UITableView,  numberOfRowsInSection section: Int) -> Int {
         //Populate two tables at the same time
@@ -52,13 +99,40 @@ extension CategoryVC: UITableViewDataSource{
         switch tableView {
         case selectedCategoryTableView:
             cell = tableView.dequeueReusableCell(withIdentifier: "reusableTVCell", for: indexPath) as! CategoryTVCell
-            cell.textLabel?.text = selectedCategories[indexPath.row]
+            cell.textLabel?.text = selectedCategories[indexPath.row].name
         case categoryTableView :
             cell = tableView.dequeueReusableCell(withIdentifier: "reusableTVCell", for: indexPath) as! CategoryTVCell
-            cell.textLabel?.text = categories[indexPath.row]
+            cell.textLabel?.text = categories[indexPath.row].name
         default:
             print("Can not populate row from array")
         }
         return cell
+    }
+}
+
+//MARK: - TableView Delegate
+extension CategoryVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        switch tableView {
+        case selectedCategoryTableView:
+            if selectedCategories[indexPath.row].checked == "check"{
+                selectedCategories[indexPath.row].checked = "uncheck"
+            }
+         
+        case categoryTableView :
+            if categories[indexPath.row].checked == "uncheck" {
+                categories[indexPath.row].checked = "check"
+            }
+        default:
+            print("Can not populate row from array")
+        }
+        
+        saveCategory()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
+        
+        
     }
 }
